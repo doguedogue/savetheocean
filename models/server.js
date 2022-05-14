11fdf6c41f1b12ab2ee17f31c3976a1ff5c140f5
Rafael Vilches <rvilches38@yahoo.com.mx>
2022-05-14 01:24:15 -0500

    add CORS

--- models/server.js
+++ models/server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require('cors')
 
 class Server {
 
@@ -14,6 +15,9 @@ class Server {
     }
 
     middleware(){
+        //CORS
+        this.app.use(cors());
+
         this.app.use ( express.static('public'));
     }
 
