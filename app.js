ec4b9890908ee73e3ce1ba1296c49df5d07b85e2
Rafael Vilches <rvilches38@yahoo.com.mx>
2022-05-15 00:29:09 -0500

    getImage v1

--- app.js
+++ app.js
@@ -11,33 +11,38 @@ const main = async() => {
 
     const busquedas = new IUCNRedList();
 
-    // Buscar Regiones
-    let data = await busquedas.getRegions( );
+    let data = null;
+    // Obtener Imagen
+    data = await busquedas.getImageByTaxonId( '3855' );
     console.log('**** Salida ****'); 
-    data.map( data => {
-        console.log('Name: ', data.name, ' identifier: ', data.identifier);
-    });
+    console.log('data: ', data);
+
+    // Buscar Regiones
+    // data = await busquedas.getRegions( );
+    // console.log('**** Salida ****'); 
+    // data.map( data => {
+    //     console.log('Name: ', data.name, ' identifier: ', data.identifier);
+    // });
 
     // Buscar Countries
-    data = await busquedas.getCountries();
-    console.log('**** Salida ****'); 
-    if (data.length > 5){
-        for(let i=0; i<5;i++)
-        console.log('isocode: ', data[i].isocode
-        , ' country: ', data[i].country);
-    }
+    // data = await busquedas.getCountries();
+    // console.log('**** Salida ****'); 
+    // if (data.length > 5){
+    //     for(let i=0; i<5;i++)
+    //     console.log('isocode: ', data[i].isocode
+    //     , ' country: ', data[i].country);
+    // }
 
 
-    // Buscar Species By Country
-    data = await busquedas.getSpeciesByCountry('MX');
-    console.log('**** Salida ****'); 
-    if (data.length > 5){
-        for(let i=0; i<5;i++)
-        console.log('taxonid: ', data[i].taxonid
-        , ' scientific_name: ', data[i].scientific_name
-        , ' category: ', data[i].category);
-    }
-
+    // // Buscar Species By Country
+    // data = await busquedas.getSpeciesByCountry('MX');
+    // console.log('**** Salida ****'); 
+    // if (data.length > 5){
+    //     for(let i=0; i<5;i++)
+    //     console.log('taxonid: ', data[i].taxonid
+    //     , ' scientific_name: ', data[i].scientific_name
+    //     , ' category: ', data[i].category);
+    // }
 
 }
 
--- models/iucn_red_list.js
+++ models/iucn_red_list.js
@@ -1,12 +1,13 @@
 const fs = require('fs');
-
 const axios = require('axios');
+var HTMLParser = require('node-html-parser');
+
 
 
 class IUCNRedList {
 
     constructor() {
-        //this.leerDB();
+        
     }
 
     get paramsIUNCRedList() {
@@ -91,6 +92,66 @@ class IUCNRedList {
         }
     }
 
+    async getImageByTaxonId( taxonid ) {
+        console.log("getImageByTaxonId");
+        const contexto = 'taxonredirect/';
+        try {
+            // Petición http
+            const intance = axios.create({
+                baseURL: `https://apiv3.iucnredlist.org/api/v3/${ contexto}${ taxonid}`,
+                params: this.paramsIUNCRedList
+            });
+            // console.log("intance", intance);
+
+            const resp = await intance.get();
+    
+            //Obtiene link a redireccionar para obtener imagen
+            const url_text = await this.getURLText(resp.data);
+
+            //Obtiene body de nueva URL
+            const div_img = await this.getBodyHTML(url_text);
+            
+            // console.log("resp.data.results", resp.data.results);
+
+            return resp.data.results.map( data => ({
+                href: url_text,
+            }));
+            
+        } catch (error) {
+            return [];
+        }
+    }
+
+    async getURLText(html_body){
+        console.log('getURLText');
+        const text_link_start = "<link href='";
+        const inicio = html_body.indexOf(text_link_start);
+        const text_link_end = "' rel='canonical'>";
+        const fin = html_body.indexOf(text_link_end);
+        
+        console.log("Inicio: ", inicio);
+        console.log("Fin: ", fin);
+        const link_html = html_body.substring(inicio + text_link_start.length, fin);
+        
+        console.log("URL HTML:", link_html);
+
+        return (link_html)? link_html.trim():''; 
+    }
+
+    async getBodyHTML(url_html = ''){
+        url_html = 'https://www.iucnredlist.org/species/3855/2878674';
+
+        axios.get(url_html, {
+            baseURL: url_html,
+            timeout: 10000
+        }).then((resp)=>{       
+            console.log("salida = ",resp.data.indexOf('featherlight'));
+            const root = HTMLParser.parse(resp.data);
+            const clase = root.querySelectorAll('featherlight');
+            console.log("clase",clase);
+
+        });
+    }
 }
 
 
